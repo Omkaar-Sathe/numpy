a02afc624f19463b0897c907c671873c571a9f5c
Omkaar-Sathe <omkaardatascientist@gmail.com>
2025-07-25 18:05:40 +0530

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,7 +1,8 @@
 
 import numpy as np
 
-arr = np.array([1, 2, 3])
+arr=np.array([1,2,3,4],dtype='s')
 
-for x in arr:
-  print(x)
+print(arr)
+
+print(arr.dtype)
--- ex2.py
+++ ex2.py
@@ -1,6 +1,9 @@
 
+
 import numpy as np
 
-arr=np.array([1,2,3,4,5,6,7])
+arr=np.array([1,2,3,4],dtype='i4')
 
-print(arr[:4])
+print(arr)
+
+print(arr.dtype)
